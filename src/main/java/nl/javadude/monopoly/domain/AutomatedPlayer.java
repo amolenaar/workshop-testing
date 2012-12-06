87a16c1e6b474987d7cad586aa1e10fdcb5d7d65
Olivier Liegeon <olivier.liegeon@imc.nl>
2012-12-06 13:10:38 +0100

    automatedPLayer

--- src/main/java/nl/javadude/monopoly/domain/AutomatedPlayer.java
+++ src/main/java/nl/javadude/monopoly/domain/AutomatedPlayer.java
@@ -1,5 +1,15 @@
 package nl.javadude.monopoly.domain;
 
-public class AutomatedPlayer
+public class AutomatedPlayer extends Player
 {
+
+    public AutomatedPlayer(Board board, String name)
+    {
+        super(board, name);
+    }
+
+    public int rollDice()
+    {
+        return 0;
+    }
 }
