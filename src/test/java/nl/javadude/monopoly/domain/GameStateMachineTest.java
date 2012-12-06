112278e9aac00c59605a08e6924f47170ec3bd7b
Olivier Liegeon <olivier.liegeon@imc.nl>
2012-12-06 10:19:32 +0100

    added testScenarios to GameStateMachine

--- src/test/java/nl/javadude/monopoly/domain/GameStateMachineTest.java
+++ src/test/java/nl/javadude/monopoly/domain/GameStateMachineTest.java
@@ -1,6 +1,73 @@
 package nl.javadude.monopoly.domain;
 
+import org.junit.*;
+
 public class GameStateMachineTest
 {
 
+    @Test
+    public void shouldNotStartPlayingWhenNoPlayersAreAdded()
+    {
+
+    }
+
+    @Test
+    public void shouldNotStartPlayingWhenGameNotStarted()
+    {
+
+    }
+
+    @Test
+    public void canStartPlayingWhenPlayersAddedGameStarted()
+    {
+
+    }
+
+    @Test
+    public void currentPlayerNotNullWhenSelectedState()
+    {
+
+    }
+
+    @Test
+    public void shouldNotSelectNextPlayerWhenCurrentPlayerIsPlaying()
+    {
+
+    }
+
+    @Test
+    public void shouldGoToJailWhenSameDiceTwice()
+    {
+
+    }
+
+    @Test
+    public void shouldNotPlayWhenInJail()
+    {
+
+    }
+
+    @Test
+    public void shouldPlayWhenThreeTurnsSpentInJail()
+    {
+
+    }
+
+    @Test
+    public void shouldBeInRolledSameOnceStateWhenFirstRollIsSameEyes()
+    {
+
+    }
+
+    @Test
+    public void shouldBeInRolledSameTwiceStateWhenSecondRollIsSameEyes()
+    {
+
+    }
+
+    @Test
+    public void shouldBeInReadyStateWhenTurnIsFinished()
+    {
+
+    }
 }
