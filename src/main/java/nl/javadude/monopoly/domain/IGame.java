22ff230325c42ed24ab3818178d913509843780e
Balazs Torma <balazs.torma@imc.nl>
2012-12-06 11:04:09 +0100

    IGame

---
+++ src/main/java/nl/javadude/monopoly/domain/IGame.java
@@ -0,0 +1,24 @@
+package nl.javadude.monopoly.domain;
+
+import java.util.*;
+
+public interface IGame
+{
+    Board getBoard();
+
+    List<ISquare> getSquares();
+
+    void addPlayer(Player player);
+
+    void startPlay();
+
+    List<Player> getPlayers();
+
+    Player getCurrentPlayer();
+
+    Player rollDice(final int d1, final int d2);
+
+    boolean currentPlayerBuy();
+
+    Player nextPlayer();
+}
