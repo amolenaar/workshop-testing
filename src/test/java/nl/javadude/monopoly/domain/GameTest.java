9706e8baf6158be33f6c185bdd356f5792f805a6
Iwein Fuld <iwein@fuld.net>
2012-12-06 09:50:21 +0100

    Cherry picks some Game tests from the JelleOlivier branch

--- src/test/java/nl/javadude/monopoly/domain/GameTest.java
+++ src/test/java/nl/javadude/monopoly/domain/GameTest.java
@@ -3,26 +3,30 @@ package nl.javadude.monopoly.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class GameTest {
-    private Game game;
+    private final Game theGame = new Game();
+    private final Player theFirstPlayer = theGame.addPlayer("myPlayer");
+    private final Player theSecondPlayer = theGame.addPlayer("myOtherPlayer");
 
     @Before
-    public void setupGame() {
-        game = new Game();
-        game.addPlayer("First");
-        game.addPlayer("Second");
-        game.startPlay();
+    public void setup()
+    {
+        theGame.startPlay();
     }
+
 
 	@Test
 	public void shouldHaveCorrectCurrentPlayer() {
-        assertThat(game.getCurrentPlayer(), is(notNullValue()));
-        assertThat(game.getCurrentPlayer().getName(), is("First"));
+        assertThat(theGame.getCurrentPlayer(), is(notNullValue()));
+        assertThat(theGame.getCurrentPlayer().getName(), is("myPlayer"));
 	}
 
     // Test the following requirement:
@@ -31,24 +35,68 @@ public class GameTest {
 
     @Test
 	public void shouldNotAllowNextPlayerIfPlayerHasntMovedYet() {
-        game.nextPlayer();
-        assertThat(game.getCurrentPlayer().getName(), is(not("Second")));
+        theGame.nextPlayer();
+        assertThat(theGame.getCurrentPlayer().getName(), is(not("myOtherPlayer")));
 	}
 
     @Test
 	public void shouldAllowNextPlayerIfPlayerHasMoved() {
-        game.move();
-        game.nextPlayer();
-        assertThat(game.getCurrentPlayer().getName(), is("Second"));
+        theGame.move();
+        theGame.nextPlayer();
+        assertThat(theGame.getCurrentPlayer().getName(), is("myOtherPlayer"));
 	}
 
     @Test
     public void shouldActivateCurrentPlayer() {
-        assertThat(game.getCurrentPlayer().isActive(), is(true));
-        game.move();
-        game.nextPlayer();
-        assertThat(game.getCurrentPlayer().isActive(), is(true));
+        assertThat(theGame.getCurrentPlayer().isActive(), is(true));
+        theGame.move();
+        theGame.nextPlayer();
+        assertThat(theGame.getCurrentPlayer().isActive(), is(true));
     }
 
+
+    @Test
+    public void previousPlayerShouldHaveEndedTurnWhenNextPlayerComesOn() {
+        final Player previousPlayer = theGame.getCurrentPlayer();
+        theGame.move();
+        theGame.nextPlayer();
+        assertThat(previousPlayer.isActive(), is(false));
+    }
+
+    @Test
+    public void currentPlayerIsNotChangedWhenPlayerDidNotMove()
+    {
+        theGame.nextPlayer();
+        verifyCurrentPlayer(theFirstPlayer);
+    }
+
+    @Test
+    public void currentPlayerIsChangedWhenPlayerHasMoved()
+    {
+        theGame.move();
+        theGame.nextPlayer();
+        verifyCurrentPlayer(theSecondPlayer);
+    }
+
+    @Test
+    public void nextPlayerMustEndTurnOfPreviousPlayer()
+    {
+        theGame.move();
+        theGame.nextPlayer();
+        assertFalse(theFirstPlayer.isActive());
+    }
+
+    @Test
+    public void nextPlayerMustStartTurnOfNewPlayer()
+    {
+        theGame.move();
+        theGame.nextPlayer();
+        assertTrue(theSecondPlayer.isActive());
+    }
+
+    private void verifyCurrentPlayer(Player aPlayer)
+    {
+        assertEquals(aPlayer, theGame.getCurrentPlayer());
+    }
 
 }
